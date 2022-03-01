20385ba256cfc9211c79b8cf7f916254d9e72ab7
Harshit <2105891@kiit.ac.in>
2022-03-01 13:21:02 +0530

    completed

--- index.js
+++ index.js
@@ -4,20 +4,21 @@ var head=document.getElementById('head');
 var cross=document.getElementById('cross');
 var poster=document.getElementById('poster-img');
 var container=document.getElementById('mega-container');
+var logoc=document.getElementsByClassName('logo-container');
 console.log(container);
 
 logo.addEventListener('click',()=>{
-
+    logo.style.display="none";
+    // logoc.style.display="none";
     container.style.display="block";
-    logo.innerHTML="";
+            console.log("logo is clicked");
      head.innerHTML="The King is here";
     poster.src="img/thumbnail.jpg";
-    console.log("logo is clicked");
     logo.style.display="none";
     
-})
+});
 cross.addEventListener('click',()=>{
     container.style.display="none";
     logo.style.display="block";
 
-})
+});
